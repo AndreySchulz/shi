b5b2c7c6e629076c96218c570339756b35c11704
Andrey_Schulz <andreygorb040693@gmail.com>
2025-10-03 21:08:19 +0300

    add svg icon in page home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,37 @@
 import HalfCircleBtn from "../../components/HalfCircleBtn/HalfCircleBtn";
 import RedActiveBtn from "../../components/RedActiveBtn/RedActiveBtn";
-import { Container, MainBlock, Navigate } from "./Home.styled";
-
+import { Container, Icon, IconBox, MainBlock, Navigate } from "./Home.styled";
+import sprite from "../../common/image/svg/sprite.svg";
 const Home = () => (
   <Container className="page">
-    <Navigate></Navigate>
+    <Navigate>
+      <Icon>
+        <use href={`${sprite}#Cup`}></use>
+      </Icon>
+      <Icon>
+        <use href={`${sprite}#HamburgerMenu`}></use>
+      </Icon>
+    </Navigate>
     <MainBlock>
       <HalfCircleBtn />
     </MainBlock>
     <MainBlock>
       <RedActiveBtn />
     </MainBlock>
+    <Navigate>
+      <IconBox>
+        <Icon>
+          <use href={`${sprite}#Spedometer`}></use>
+        </Icon>
+        <p>11111</p>
+      </IconBox>
+      <IconBox>
+        <Icon>
+          <use href={`${sprite}#Heart`}></use>
+        </Icon>
+        <p>8/10</p>
+      </IconBox>
+    </Navigate>
   </Container>
 );
 
