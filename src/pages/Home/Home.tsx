a61fff896c7df08f01c4bd97b913df8598a1f960
Andrey_Schulz <andreygorb040693@gmail.com>
2025-10-03 18:47:33 +0300

    add svg

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,17 @@
+import HalfCircleBtn from "../../components/HalfCircleBtn/HalfCircleBtn";
+import RedActiveBtn from "../../components/RedActiveBtn/RedActiveBtn";
+import { Container, MainBlock, Navigate } from "./Home.styled";
 
+const Home = () => (
+  <Container className="page">
+    <Navigate></Navigate>
+    <MainBlock>
+      <HalfCircleBtn />
+    </MainBlock>
+    <MainBlock>
+      <RedActiveBtn />
+    </MainBlock>
+  </Container>
+);
 
-
-const Home = () => ('')
- 
-
-export default Home
+export default Home;
