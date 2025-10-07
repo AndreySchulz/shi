030c1c78fb355376d75c07c97a06ff64c4204583
Andrey_Schulz <andreygorb040693@gmail.com>
2025-10-07 13:38:06 +0300

    add header

--- src/pages/Connect/Connect.tsx
+++ src/pages/Connect/Connect.tsx
@@ -1,10 +1,11 @@
-import PageLayout from '../../components/PageLayout'
+import Header from "../../components/Header/Header";
 
-const Connect = () => (
-  <PageLayout
-    title="Connect"
-    caption="Connection instructions and join codes will be added to this screen."
-  />
-)
+const Connect = () => {
+  return (
+    <div className="page">
+      <Header />
+    </div>
+  );
+};
 
-export default Connect
+export default Connect;
