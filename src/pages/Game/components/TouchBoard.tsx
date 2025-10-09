7e8fa8a57cac8290d7d981d3dfaeae1e3e103ede
Andrey_Schulz <andreygorb040693@gmail.com>
2025-10-09 13:14:09 +0300

    edit RedBtn

--- src/pages/Game/components/TouchBoard.tsx
+++ src/pages/Game/components/TouchBoard.tsx
@@ -1,20 +1,33 @@
-const TouchBoard = ({ displayValue, ariaLive = 'off' }: {
-  displayValue: string | number
-  ariaLive?: 'off' | 'polite' | 'assertive'
+import RedActiveBtn from "../../../components/RedActiveBtn/RedActiveBtn";
+
+const TouchBoard = ({
+  displayValue,
+  ariaLive = "off",
+}: {
+  displayValue: string | number;
+  ariaLive?: "off" | "polite" | "assertive";
 }) => (
   <div className="game__touch">
     <div className="game__touch-row">
-      <span className="game__touch-point" aria-hidden />
-      <span className="game__touch-point" aria-hidden />
+      <span className="game__touch-point" aria-hidden>
+        <RedActiveBtn />
+      </span>
+      <span className="game__touch-point" aria-hidden>
+        <RedActiveBtn />
+      </span>
     </div>
     <div className="game__touch-countdown" aria-live={ariaLive}>
       {displayValue}
     </div>
     <div className="game__touch-row">
-      <span className="game__touch-point" aria-hidden />
-      <span className="game__touch-point" aria-hidden />
+      <span className="game__touch-point" aria-hidden>
+        <RedActiveBtn />
+      </span>
+      <span className="game__touch-point" aria-hidden>
+        <RedActiveBtn />
+      </span>
     </div>
   </div>
-)
+);
 
-export default TouchBoard
+export default TouchBoard;
